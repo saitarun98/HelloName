64c6f90821a392539718f551b3074b5ac5950a4b
Saitarun Irigala <saitarun@mail.usf.edu>
2019-01-15 17:53:58 -0500

    In-Clas Assignment

--- HelloName/Program.cs
+++ HelloName/Program.cs
@@ -6,7 +6,9 @@ namespace HelloName
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Saitarun Irigala";
+            Console.WriteLine(name);
+            Console.ReadKey(true);
         }
     }
 }
